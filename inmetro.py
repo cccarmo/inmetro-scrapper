c577ddf627d7de1bb4d9c9c6f3b124394cfe60bc
Rafael Batista Carmo <rafael-batista.carmo@viseo.com>
2021-11-19 23:47:19 +0100

    finish v1 of inmetro scrapper

--- inmetro.py
+++ inmetro.py
@@ -1,16 +1,12 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import codecs
 import xlsxwriter
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-
 # Open main page
 url = "http://www.inmetro.gov.br/prodcert/certificados/busca.asp"
-service = Service('C:/Users/RafaelBatistaCarmo/Documents/Personal/inmetro/chromedriver.exe')
+service = Service('chromedriver.exe')
 driver = webdriver.Chrome(service=service)
 driver.get(url)
 
@@ -23,7 +19,7 @@ search_button = driver.find_element(By.NAME, "btn_enviar")
 search_button.click()
 
 
-workbook = xlsxwriter.Workbook('teste.xlsx')
+workbook = xlsxwriter.Workbook('InmetroTable.xlsx')
 worksheet = workbook.add_worksheet()
 worksheet.write(0, 0, 'Informações gerais')
 worksheet.write(0, 1, 'CNPJ/CPF')
@@ -48,53 +44,77 @@ nome_fantasia = None
 endereco = None
 status = None
 papel_da_empresa = None
+starts_on_page = 1
+total_pages = 38
 
-
-for i in range(38):
-
-    # Wait for the load
+if starts_on_page > 1:
     driver.implicitly_wait(30)
-    # Gambiarra pra esperar
-    page_lenght = len(driver.find_elements(By.CLASS_NAME, "listagem"))
-    while page_lenght < 30 and i < 37:
-        driver.implicitly_wait(5)
-        page_lenght = len(driver.find_elements(By.CLASS_NAME, "listagem"))
+    driver.execute_script("Pagina("+ str(starts_on_page) + ", 1, 10)")
+
+
+for i in range(starts_on_page - 1, total_pages):
+    driver.implicitly_wait(30)
 
     for a in driver.find_elements(By.CLASS_NAME,"listagem"):
         if  'Certificador:' in a.text:
             print(a.text)
             informacoes_gerais = a.text
             basic_info = 0
+            cnpj = None
+            razao_social = None
+            nome_fantasia = None
+            endereco = None
+            status = None
+            papel_da_empresa = None
             continue
         elif basic_info == 0:
             basic_info += 1
             cnpj = a.text
         elif basic_info == 1:
             basic_info += 1
-            razao_social = a.text
+            if razao_social:
+                razao_social = razao_social + ' / ' + a.text
+            else:
+                razao_social = a.text
         elif basic_info == 2:
             basic_info += 1
-            nome_fantasia = a.text
+            if nome_fantasia:
+                nome_fantasia = nome_fantasia + ' / ' + a.text
+            else:
+                nome_fantasia = a.text
         elif basic_info == 3:
             basic_info += 1
-            endereco = a.text
+            if endereco:
+                endereco = endereco + ' / ' + a.text
+            else:
+                endereco = a.text
         elif basic_info == 4:
             basic_info += 1
-            status = a.text
+            if status:
+                status = status + ' / ' + a.text
+            else:
+                status = a.text
         elif basic_info == 5:
             basic_info += 1
-            papel_da_empresa = a.text
+            if papel_da_empresa:
+                papel_da_empresa = papel_da_empresa + ' / ' + a.text
+            else:
+                papel_da_empresa = a.text
         elif basic_info == 6:
-            basic_info += 1
-            worksheet.write(row, 0, informacoes_gerais)
-            worksheet.write(row, 1, cnpj)
-            worksheet.write(row, 2, razao_social)
-            worksheet.write(row, 3, nome_fantasia)
-            worksheet.write(row, 4, endereco)
-            worksheet.write(row, 5, status)
-            worksheet.write(row, 6, papel_da_empresa)
-            col = 7
-            worksheet.write(row, col, a.text)
+            if  a.text[:11].isdigit():
+                cnpj = cnpj + ' / ' + a.text
+                basic_info = 1
+            else:
+                basic_info += 1
+                worksheet.write(row, 0, informacoes_gerais)
+                worksheet.write(row, 1, cnpj)
+                worksheet.write(row, 2, razao_social)
+                worksheet.write(row, 3, nome_fantasia)
+                worksheet.write(row, 4, endereco)
+                worksheet.write(row, 5, status)
+                worksheet.write(row, 6, papel_da_empresa)
+                col = 7
+                worksheet.write(row, col, a.text)
         elif basic_info > 6 and basic_info < 9:
             basic_info += 1
             col += 1
@@ -105,7 +125,13 @@ for i in range(38):
             row += 1
             basic_info = 6
 
-    next_button = driver.find_element(By.XPATH,  "//*[contains(text(),'Próximo >>')]")
-    next_button.click()
+    if i < total_pages - 1:
+        try:
+            driver.execute_script("Pagina("+ str(i + 2) + ", 1, 10)")
+        except:
+            worksheet.write(row + 1, 0, 'Foi ate a pagina: ' + str(i + 1))
+            break
+
+
 
 workbook.close()
